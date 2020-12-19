9532dcdebfb6cc07105177938dc5350f9e4f6a69
My name <me@example.com>
2020-12-19 22:10:21 +0800

    version4

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -131,7 +131,7 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       document.querySelector('#main-content')
-      .innerHTML = homeHTML;
+      .innerHTML = homeHtml;
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
