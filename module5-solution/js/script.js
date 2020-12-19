aa413bbb1dcacde6ff85cefe2ed8f9e4c7578045
My name <me@example.com>
2020-12-19 22:03:35 +0800

    version3

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -130,7 +130,8 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
+      document.querySelector('#main-content')
+      .innerHTML = homeHTML;
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
